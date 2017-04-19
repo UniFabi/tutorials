6ece754810165490f9ed0e43dd49ddb4cb21ca0a
FabiDaheim <fabian.schober@web.de>
2017-04-19 18:30:08 +0200

    Fractals Tutorial bis Punkt 5

---
+++ tutorial_02_fractals/Assets/scripts/Fractal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+    public Mesh mesh;
+    public Material material;
+    public int maxDepth;
+    public float childScale;
+
+    private int depth;
+
+    public void Start() {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+
+        if(depth<maxDepth){
+            new GameObject("Fractal Child "+depth).AddComponent<Fractal>().Initialize(this, Vector3.up);
+            new GameObject("Fractal Child "+depth).AddComponent<Fractal>().Initialize(this, Vector3.right);
+        }
+
+    }
+    
+    private void Initialize (Fractal parent, Vector3 direction) {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        childScale = parent.childScale;
+        depth = parent.depth + 1;
+
+        //Creates hierarchical parent-children objects
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (0.5f + 0.5f * childScale);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
